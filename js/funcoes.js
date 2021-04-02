b138ee907b54581ed7de29cfc6704cdd4dde6741
Your Name <suellenwmj@hotmail.com>
2021-04-01 22:26:37 -0300

    Lista de FIlmes

---
+++ js/funcoes.js
@@ -0,0 +1,17 @@
+let filmes = ["Soul" , "Onde esta Segunda" , "Simplesmente acontece"];
+console.log(filmes);
+console.log(filmes.length);
+
+//console.log(filmes[0]);
+//console.log(filmes[1]);
+//console.log(filmes[2]);
+
+let quantidadeFilmes = 3;
+while(quantidadeFilmes >= 0){
+console.log(filmes[quantidadeFilmes]);
+quantidadeFilmes==;
+
+
+}
+
+
